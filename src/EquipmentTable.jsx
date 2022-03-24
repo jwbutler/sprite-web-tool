9c90eccc99339254a1c88288ae94b3b9817f60ad
Jack Butler <john.butler@uconn.edu>
2022-03-24 19:57:28 -0400

    Transition the project to NPM+Typescript

--- src/EquipmentTable.jsx
+++ src/EquipmentTable.jsx
@@ -1,28 +1,29 @@
-{
-  const EquipmentTable = ({equipment, enabledEquipment, onChange}) => (
-    <div className="EquipmentTable">
-      {
-        equipment.sort()
-          .map(item => (
-            <div className="row">
-              <div className="col">
-                {item}
-                </div>
-              <div className="col">
-                <input
-                  type="checkbox"
-                  name="equipment"
-                  value={item}
-                  checked={enabledEquipment.indexOf(item) > -1}
-                  onChange={e => onChange(e)}
-                  key={item}
-                />
-              </div>
-            </div>
-          ))
-      }
-    </div>
-  );
+import React from 'react';
+import './EquipmentTable.css';
 
-  window.jwb.EquipmentTable = EquipmentTable;
-}
+const EquipmentTable = ({ equipment, enabledEquipment, onChange }) => (
+  <div className="EquipmentTable">
+    {
+      equipment.sort()
+        .map(item => (
+          <div className="row">
+            <div className="col">
+              {item}
+              </div>
+            <div className="col">
+              <input
+                type="checkbox"
+                name="equipment"
+                value={item}
+                checked={enabledEquipment.indexOf(item) > -1}
+                onChange={e => onChange(e)}
+                key={item}
+              />
+            </div>
+          </div>
+        ))
+    }
+  </div>
+);
+
+export default EquipmentTable;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
-{
-  const { SpriteTool } = window.jwb;
+import SpriteTool from './SpriteTool';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Hello, World!');
   const container = document.getElementById('CONTAINER');
-  ReactDOM.render(<SpriteTool />, document.getElementById('CONTAINER'));
-}
+  ReactDOM.render(<SpriteTool />, container);
+});
